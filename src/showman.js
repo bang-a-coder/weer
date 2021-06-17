8bba1f6903c24807348de1da2cd4c18b7785b07b
bang-a-coder <nikitas4717@gmail.com>
2021-06-17 23:53:03 +0200

    weather icons

--- src/showman.js
+++ src/showman.js
@@ -6,11 +6,16 @@ export function showman(obj){
 	let temp = document.querySelector('#temp')
 	let feelsLike = document.querySelector('#feels-like')
 	let cityName = document.querySelector('#city-name')
-	let cloud = document.querySelector('.cloud')
+	let cloudDesc = document.querySelector('.cloud-desc')
 	let wind = document.querySelector('.wind')
+	let cloudIcon = document.querySelector('.cloud-icon')	
+	console.log(cloudIcon)
 
 	temp.innerHTML = obj.main.temp + '°'
 	feelsLike.innerHTML = obj.main.feels_like
 	cityName.innerHTML = obj.name + countryCodeEmoji(obj.sys.country)
-	cloud.innerHTML = obj.weather[0].description
+	cloudDesc.innerHTML = obj.weather[0].description
+	cloudIcon.src = `http://openweathermap.org/img/wn/${obj.weather[0].icon}@2x.png`
+
+	console.log(obj.weather[0].icon)
 }
